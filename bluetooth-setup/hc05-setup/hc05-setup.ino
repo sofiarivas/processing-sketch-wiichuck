e0d6b6e2300daa9f5b0cdf5db9ba685416e7ed29
Sofía Rivas <sofi.rivasm@gmail.com>
2017-01-07 23:34:08 -0600

    Add arduino bluetooth setup files

---
+++ bluetooth-setup/hc05-setup/hc05-setup.ino
@@ -0,0 +1,38 @@
+#include <Wiichuck.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+Wiichuck wii;
+SoftwareSerial BTSerial(10, 11); // RX | TX
+int value = 0;
+
+void setup()
+{
+  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(9, HIGH);
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  BTSerial.begin(9600);  // HC-05 default speed in AT command mode
+
+  wii.init();
+  wii.calibrate();  // calibration
+}
+
+void loop()
+{
+
+  if (wii.poll()) {
+    // This will output a booelan value to check if wii nunchuck is sending data
+    value = wii.buttonC();
+    // To make setup easier you can print the value to confirm you are getting data
+    //Serial.println(value);
+
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+    
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+  }} 
